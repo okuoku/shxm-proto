1911a9861350f6e89d895e6737c17705e9db279e
okuoku <mjt@cltn.org>
2021-05-02 03:30:01 +0900

    compile to SPIRV

--- driver.c
+++ driver.c
@@ -3,9 +3,28 @@
 
 void shxm_init(void);
 void shxm_deinit(void);
+void* shxm_build(int mode, const char* source);
+
+static void*
+readfile(const char* fn){
+    FILE* fp;
+    size_t sz;
+    char* r;
+    fp = fopen(fn, "rb");
+    fseek(fp, 0, SEEK_END);
+    sz = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    r = malloc(sz+1);
+    r[sz] = 0;
+    fread(r, sz, 1, fp);
+    return r;
+}
 
 int
 main(int ac, char** av){
+    char* src;
     shxm_init();
+    src = readfile(SOURCEPATH "/physpre.frag");
+    shxm_build(0 /* frag */, src);
     shxm_deinit();
 }
--- interface.cpp
+++ interface.cpp
@@ -1,11 +1,77 @@
 #include "ShaderLang.h"
+#include "SPIRV/GlslangToSpv.h"
+#include "SPIRV/disassemble.h"
+#include <iostream>
+
+namespace glslang {
+    extern TBuiltInResource DefaultTBuiltInResource;
+};
 
 extern "C" void
 shxm_init(void){
-    (void)ShInitialize();
+    (void)glslang::InitializeProcess();
 }
 
 extern "C" void
 shxm_deinit(void){
-    (void)ShFinalize();
+    (void)glslang::FinalizeProcess();
+}
+
+extern "C" void*
+shxm_build(int mode, const char* source){
+    glslang::TShader::ForbidIncluder includer;
+    glslang::TShader* ts;
+    switch(mode){
+        case 0:
+            ts = new glslang::TShader(EShLangFragment);
+            break;
+        case 1:
+            ts = new glslang::TShader(EShLangVertex);
+            break;
+        default:
+            return 0;
+    }
+
+    ts->setStrings(&source, 1);
+    ts->setAutoMapBindings(true);
+    ts->setAutoMapLocations(true);
+    // ts->setInvertY(true);
+    ts->setEnvInput(glslang::EShSourceGlsl,
+                    (mode == 0)?EShLangFragment : EShLangVertex,
+                    glslang::EShClientVulkan,
+                    100);
+    ts->setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_0);
+    ts->setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
+
+    if(ts->parse(&glslang::DefaultTBuiltInResource,
+                 100,
+                 false,
+                 EShMsgDefault,
+                 includer)){
+        std::vector<unsigned int> spirv;
+        spv::SpvBuildLogger logger;
+        glslang::SpvOptions opt;
+
+        printf("Success.\n%s\n%s\n",
+               ts->getInfoLog(),
+               ts->getInfoDebugLog());
+        glslang::GlslangToSpv(*ts->getIntermediate(),
+                              spirv,
+                              &logger,
+                              &opt);
+        printf("%s\n", logger.getAllMessages().c_str());
+        spv::Disassemble(std::cout, spirv);
+    }else{
+        printf("Fail.\n%s\n%s\n",
+               ts->getInfoLog(),
+               ts->getInfoDebugLog());
+    }
+
+
+
+    return static_cast<void*>(ts);
+}
+
+extern "C" void
+shxm_release(void* obj){
 }
